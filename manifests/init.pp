bda23de0985c401b597fefe6be41232fe7293400
Aron Roberts <aron@socrates.berkeley.edu>
2013-12-05 12:40:02 -0800

    CSPACE-6273: Create namespace for system temporary directory identification code.

--- manifests/init.pp
+++ manifests/init.pp
@@ -12,33 +12,6 @@
 #
 class cspace_environment {
 	
-	# ---------------------------------------------------------
-	# Identify the system temporary directory
-	# ---------------------------------------------------------
+	include cspace_environment::tempdir
 	
-	# The environment variables below have been added as custom Facter facts
-	# via the lib/facter/env.rb script in this module.
-	
-	$tmpdir = $env_tmpdir # Unix, Linux, and OS X
-	$temp = $env_temp # Microsoft Windows
-	notify { "Unix/Linux/OS X TMPDIR is ${tmpdir}": }
-	notify { "Windows TEMP is ${temp}": }
-	
-	# FIXME: This default/fallback directory is only
-	# relevant for most Unix-like systems.  See
-	# http://en.wikipedia.org/wiki/Temporary_folder
-	# for the fallback temporary director(ies) for Windows.
-	$default_temporary_directory = '/tmp'
-	
-	if $tmpdir != undef {
-        $system_temp_directory = $tmpdir
-	}
-	elsif $temp != undef {
-        $system_temp_directory = $temp
-	}
-	else {
-        $system_temp_directory = $default_temporary_directory
-	}
-	notify { "Temporary directory is ${system_temp_directory}": }
-
 }
