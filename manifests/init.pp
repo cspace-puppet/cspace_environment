2799aad368a42970c449c57be98259b95da2838c
Aron Roberts <aron@socrates.berkeley.edu>
2013-12-05 15:23:23 -0800

    CSPACE-6273: Commented out platform-specific contents in init.pp until we can run these sensibly.

--- manifests/init.pp
+++ manifests/init.pp
@@ -11,24 +11,29 @@
 # Sample Usage:
 #
 class cspace_environment {
+    
+    # Create a 'cspace' user account
+	# user { 'Create cspace user account':
+	# 	ensure  => present,
+	# 	name    => 'cspace',
+	# 	# home => '/home/cspace', # System-dependent?
+	# 	# managehome => true, # Not available with 'directoryservice' provider
+	# }
+  
+  	# Create a (currently placeholder) set of CollectionSpace
+	# server-related environment variables for all users.
 	
-  user {'cspace':
-    ensure => present,
-    home => '/home/cspace',
-    managehome => true,
-  }
+	# file { 'Create global environment variables':
+	# 	path   => '/etc/profile.d/cspace_environment.sh'
+	# 	source => 'puppet:///modules/cspace_environment/cspace_environment.sh'
+	# }
   
-  file {'/etc/profile.d/cspace_environment.sh':
-    source => 'puppet:///modules/cspace_environment/cspace_environment.sh'
-  }
-  
-  file {'/usr/local/bin/papply.sh':
-    source => 'puppet:///modules/cspace_environment/papply.sh'
-  }
-  
-  exec { 'chmod ugo+x papply.sh':
-    cwd     => "/usr/local/bin",
-    path    => ["/bin", "/usr/bin", "/usr/sbin"],
-    require => File["chmod ugo+x papply.sh"]
-  }
+    # Create a convenience shell script for launching Puppet
+	# and running the main CollectionSpace site.pp manifest.
+	# file { 'Create Puppet CollectionSpace launch script':
+	# 	path   => '/usr/local/bin/papply.sh',
+	# 	source => 'puppet:///modules/cspace_environment/papply.sh',
+	# 	mode   => 'ugo+rx',
+	# }
+	
 }
