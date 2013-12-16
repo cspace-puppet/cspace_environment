85fd68bc0dffa214b28c23226171afb8166bf4c4
Aron Roberts <aron@socrates.berkeley.edu>
2013-12-16 15:38:43 -0800

    CSPACE-6273: Minor formatting change in one test.

--- tests/env.pp
+++ tests/env.pp
@@ -3,4 +3,4 @@ include stdlib
 class { 'cspace_environment::env': }
 
 $env_vars = join( sort ($cspace_environment::env::cspace_env_vars), "\n" )
-notice( "env_vars_list=${env_vars}" )
+notice( "CollectionSpace-relevant environment variables consist of:\n${env_vars}" )
