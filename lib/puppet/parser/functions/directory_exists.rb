cf576614862860a3e17aecd2ce93d5ae569c2e93
Aron Roberts <aron@socrates.berkeley.edu>
2014-01-08 19:25:13 -0800

    CSPACE-6273: Identified that directory_exists function returns a value; added documentation and error checking to function.

--- lib/puppet/parser/functions/directory_exists.rb
+++ lib/puppet/parser/functions/directory_exists.rb
@@ -6,13 +6,26 @@
 # http://docs.puppetlabs.com/guides/plugins_in_modules.html
 
 module Puppet::Parser::Functions
-  newfunction(:directory_exists) do |args|
+  newfunction(:directory_exists, :type => :rvalue, :doc => <<-ENDDOC
+Identifies whether a directory exists, given its path
+
+*Examples:*
+
+    directory_exists('/etc/motd')
+
+Will return (on relevant systems):
+
+    true
+ENDDOC
+  ) do |args|
+    raise(Puppet::ParseError, "directory_exists(): Wrong number of arguments " +
+      "given (got #{args.size} but expected 1)") if (args.size < 1 or args.size > 1)
     dirpath = args[0]
     if File.exists?(dirpath) && File.directory?(dirpath)
       returnval = true
     else
       returnval = false
     end
-    returnval
+    return returnval
   end
 end
